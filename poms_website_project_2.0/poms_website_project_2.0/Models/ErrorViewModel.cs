ee0fd95e867cea10db8b324093a95247a54b1e33
03yolo <lorenzemari.uni@gmail.com>
2025-07-30 19:44:43 +0800

    Created new project
    7/30/2025
    7:44 PM

---
+++ poms_website_project_2.0/poms_website_project_2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace poms_website_project_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
