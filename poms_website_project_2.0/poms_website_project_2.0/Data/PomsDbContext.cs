5b15d70cce44be4965938f7d1ffdb8071807c0c6
03yolo <lorenzemari.uni@gmail.com>
2025-08-03 23:06:54 +0800

    -

--- poms_website_project_2.0/poms_website_project_2.0/Data/PomsDbContext.cs
+++ poms_website_project_2.0/poms_website_project_2.0/Data/PomsDbContext.cs
@@ -19,4 +19,33 @@ public DbSet<poms_website_project_2._0.Models.LearnerModel> LearnerModel { get; 
 public DbSet<poms_website_project_2._0.Models.FacultyModel> FacultyModel { get; set; } = default!;
 
 public DbSet<poms_website_project_2._0.Models.AssessmentModel> AssessmentModel { get; set; } = default!;
+
+    public DbSet<GradeModel> GradeModel { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<GradeModel>(entity =>
+        {
+            entity.HasKey(e => e.GradeId);
+
+            entity.HasOne(g => g.Learner)
+                  .WithMany()
+                  .HasForeignKey(g => g.LearnerId)
+                  .OnDelete(DeleteBehavior.Restrict); // Important!
+
+            entity.HasOne(g => g.Faculty)
+                  .WithMany()
+                  .HasForeignKey(g => g.FacultyId)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(g => g.Subject)
+                  .WithMany()
+                  .HasForeignKey(g => g.SubjectId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        });
     }
+
+}
+
