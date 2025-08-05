e85e6d2ad9f40a5df15b724ca2642d9ef7a56131
AMMIEL JAMIR <JAV0226@dlsud.edu.ph>
2025-08-05 18:50:31 +0800

    Added Dashboard Controller, _Dashboard.cshtml, and images folder
    Minor Changes on Index.cshtml of models
    8/05/2025
    6:43 PM

---
+++ poms_website_project_2.0/poms_website_project_2.0/Controllers/DashboardController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace poms_website_project_2._0.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // Alert Repository Folder here [To be Added]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Checks the UserRoles and return View based on their role. ex. AdminDashboard.cshthml
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AdminDashboard()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Faculty")]
+        public async Task<IActionResult> FacultyDashboard()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Learner")]
+        public async Task<IActionResult> LearnerDashboard()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Parent")]
+        public async Task<IActionResult> ParentDashboard()
+        {
+            return View();
+        }
+
+
+    }
+}
