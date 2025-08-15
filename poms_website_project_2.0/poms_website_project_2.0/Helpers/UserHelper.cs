5c58f9a62bf7f4f6bff9e1997658b2a5fd7371be
03yolo <lorenzemari.uni@gmail.com>
2025-08-15 23:17:46 +0800

    Modified user helper
    8/15/2025
    11:17 PM

--- poms_website_project_2.0/poms_website_project_2.0/Helpers/UserHelper.cs
+++ poms_website_project_2.0/poms_website_project_2.0/Helpers/UserHelper.cs
@@ -1,6 +1,65 @@
-﻿namespace poms_website_project_2._0.Helpers
+﻿using Microsoft.AspNetCore.Identity;
+using poms_website_project_2._0.Models;
+using poms_website_project_2._0.Repositories;
+using poms_website_project_2._0.Data.Entities;
+
+
+namespace poms_website_project_2._0.Helpers
 {
     public class UserHelper
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ILearnerRepository _learnerRepository;
+
+        public UserHelper(
+           UserManager<User> userManager,
+           SignInManager<User> signInManager,
+           RoleManager<IdentityRole> roleManager,
+           ILearnerRepository  learnerRepository)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _learnerRepository = learnerRepository;
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginModel model)
+        {
+            return await _signInManager.PasswordSignInAsync(
+                model.LoginID,
+                model.Password,
+                model.RememberMe,
+                false);
+        }
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> AddUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task AddUserToRoleAsync(User user, string roleName)
+        {
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
+
+        public async Task AddUserToRoleAsync(User user, int roleId)
+        {
+            string roleName = roleId switch
+            {
+                1 => "Admin",
+                2 => "Faculty",
+                3 => "Parent",
+                4 => "Learner",
+                _ => throw new ArgumentException("Invalid RoleId")
+            };
+
+            await _userManager.AddToRoleAsync(user, roleName);
+        }
     }
 }
--- poms_website_project_2.0/poms_website_project_2.0/obj/Debug/net8.0/poms_website_project_2.0.AssemblyInfo.cs
+++ poms_website_project_2.0/poms_website_project_2.0/obj/Debug/net8.0/poms_website_project_2.0.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6477f807f5eb793ec2f2a6d4cb4b6465cded15a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213f6def4cdcaa515519bba6710aa9927a59663f")]
 [assembly: System.Reflection.AssemblyProductAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
