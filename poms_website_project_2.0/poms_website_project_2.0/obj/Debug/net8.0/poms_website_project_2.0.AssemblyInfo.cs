a4ba2af9440dd198cab6a9e736f8b7f141482161
AMMIEL JAMIR <JAV0226@dlsud.edu.ph>
2025-08-26 23:38:37 +0800

    Added dashboard.css and Edited Dashboard
    8/26/2025
    11:38 PM

--- poms_website_project_2.0/poms_website_project_2.0/obj/Debug/net8.0/poms_website_project_2.0.AssemblyInfo.cs
+++ poms_website_project_2.0/poms_website_project_2.0/obj/Debug/net8.0/poms_website_project_2.0.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ddef3aa1f089cdc0a4872b925422ea083efb4e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db288bd575bc18b484a0195b0da59072cb54ecca")]
 [assembly: System.Reflection.AssemblyProductAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poms_website_project_2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
